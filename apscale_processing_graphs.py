fa53c126b1f7bc1f6fd3757833a40e11c2cb03dd
hempelc <hempelc@uoguelph.ca>
2023-08-17 14:52:43 -0400

    first commit

---
+++ apscale_processing_graphs.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python3
+
+"""
+A script to generate processing graphs for apscale runs. Must be started from the
+parent directory of an apscale project directory.
+
+Requirements: apscale project directory that contains a Project_report.xlsx file
+with sheets named 3_PE merging, 4_primer_trimming, and 5_quality_filtering,
+as well as the folders 7_otu_clustering, 8_denoising, and 9_lulu_filtering
+with all result files.
+
+By Chris Hempel (christopher.hempel@kaust.edu.sa) on Jun 7 2023
+"""
+
+import os
+import pandas as pd
+import plotly.express as px
+import argparse
+import warnings
+
+# Define that warnings are not printed to console
+warnings.filterwarnings("ignore")
+
+# Define arguments
+parser = argparse.ArgumentParser(
+    description="""A script to generate processing graphs for apscale runs.""",
+)
+parser.add_argument(
+    "-p",
+    "--project_name",
+    help="Name of the apscale project to generate reports for (without the _apscale suffix).",
+    required=True,
+)
+parser.add_argument(
+    "-f",
+    "--graph_format",
+    help="Graph format, either png or svg.",
+    required=True,
+    choices=["png", "svg"],
+)
+parser.add_argument(
+    "-w",
+    "--graph_width",
+    help="Graph width (default=2500).",
+    default=2500,
+    type=int,
+)
+parser.add_argument(
+    "-t",
+    "--ticklabel_size",
+    help="Tick label font size (default=7).",
+    default=7,
+    type=int,
+)
+args = parser.parse_args()
+
+# Set arguments
+project_name = args.project_name
+graph_width = args.graph_width
+ticksize = args.ticklabel_size
+graph_format = args.graph_format
+
+# os.chdir("/Users/christopherhempel/Desktop/apscale_wrapper")
+# project_name = "test"
+# graph_width = 2500
+# ticksize = 7
+# graph_format = "png"
+
+
+# Import files
+report_file = os.path.join(f"{project_name}_apscale", "Project_report.xlsx")
+esv_postlulu_file = os.path.join(
+    f"{project_name}_apscale",
+    "9_lulu_filtering",
+    "denoising",
+    f"{project_name}_apscale_ESV_table_filtered.xlsx",
+)
+esv_prelulu_file = os.path.join(
+    f"{project_name}_apscale", "8_denoising", f"{project_name}_apscale_ESV_table.xlsx"
+)
+otu_postlulu_file = os.path.join(
+    f"{project_name}_apscale",
+    "9_lulu_filtering",
+    "otu_clustering",
+    f"{project_name}_apscale_OTU_table_filtered.xlsx",
+)
+otu_prelulu_file = os.path.join(
+    f"{project_name}_apscale",
+    "7_otu_clustering",
+    f"{project_name}_apscale_OTU_table.xlsx",
+)
+report_sheet_dict = pd.read_excel(report_file, sheet_name=None)
+esv_postlulu_df = pd.read_excel(esv_postlulu_file, "ESV table")
+esv_prelulu_df = pd.read_excel(esv_prelulu_file, "ESV table")
+otu_postlulu_df = pd.read_excel(otu_postlulu_file, "OTU table")
+otu_prelulu_df = pd.read_excel(otu_prelulu_file, "OTU table")
+
+
+# ESV table processing
+esv_postlulu_df = esv_postlulu_df.drop("Seq", axis=1).set_index("ID")
+esv_postlulu_df[esv_postlulu_df > 1] = 1
+esv_postlulu_sums = esv_postlulu_df.sum()
+esv_postlulu_sums.name = "ESVs postlulu"
+esv_postlulu_sums.index = (
+    esv_postlulu_sums.index.str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace("_filtered", "")
+    .str.replace("_dereplicated", "")
+)
+esv_prelulu_df = esv_prelulu_df.drop("Seq", axis=1).set_index("ID")
+esv_prelulu_df[esv_prelulu_df > 1] = 1
+esv_prelulu_sums = esv_prelulu_df.sum()
+esv_prelulu_sums.name = "ESVs prelulu"
+esv_prelulu_sums.index = (
+    esv_prelulu_sums.index.str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace("_filtered", "")
+    .str.replace("_dereplicated", "")
+)
+# OTU table processing
+otu_postlulu_df = otu_postlulu_df.drop("Seq", axis=1).set_index("ID")
+otu_postlulu_df[otu_postlulu_df > 1] = 1
+otu_postlulu_sums = otu_postlulu_df.sum()
+otu_postlulu_sums.name = "OTUs postlulu"
+otu_postlulu_sums.index = (
+    otu_postlulu_sums.index.str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace("_filtered", "")
+    .str.replace("_dereplicated", "")
+)
+otu_prelulu_df = otu_prelulu_df.drop("Seq", axis=1).set_index("ID")
+otu_prelulu_df[otu_prelulu_df > 1] = 1
+otu_prelulu_sums = otu_prelulu_df.sum()
+otu_prelulu_sums.name = "OTUs prelulu"
+otu_prelulu_sums.index = (
+    otu_prelulu_sums.index.str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace("_filtered", "")
+    .str.replace("_dereplicated", "")
+)
+
+# Graphs
+# Set graph width based on njumber of samples (each bar should be 20 wide)
+graph_width = len(report_sheet_dict["3_PE merging"]) * 20
+
+# PE merging
+perc_kept_pe = pd.Series(
+    report_sheet_dict["3_PE merging"]["merged reads"]
+    / report_sheet_dict["3_PE merging"]["processed reads"]
+    * 100
+)
+perc_kept_pe.index = (
+    report_sheet_dict["3_PE merging"]["File"]
+    .str.replace("_PE", "")
+    .str.replace(".fastq.gz", "")
+)  # type: ignore
+perc_kept_pe = perc_kept_pe.fillna(0)
+pe_graph = px.bar(
+    perc_kept_pe.sort_values(),
+    range_y=[0, 100],
+    labels={"value": "Reads kept [%]", "File": "Samples"},
+    title="Percentage of reads kept during PE merging",
+    width=graph_width,
+)
+pe_graph.add_hline(
+    y=(sum(perc_kept_pe) / len(perc_kept_pe)),
+    line_width=1,
+    line_dash="dash",
+)
+pe_graph.update_layout(showlegend=False)
+pe_graph.update_xaxes(tickangle=55)
+
+# Primer trimming
+perc_kept_trim = pd.Series(
+    report_sheet_dict["4_primer_trimming"]["trimmed reads"]
+    / report_sheet_dict["4_primer_trimming"]["processed reads"]
+    * 100
+)
+perc_kept_trim.index = (
+    report_sheet_dict["4_primer_trimming"]["File"]
+    .str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace(".fastq.gz", "")
+)  # type: ignore
+perc_kept_trim = perc_kept_trim.fillna(0)
+trim_graph = px.bar(
+    perc_kept_trim.sort_values(),
+    range_y=[0, 100],
+    labels={"value": "Reads kept [%]", "File": "Samples"},
+    title="Percentage of reads kept during trimming",
+    width=graph_width,
+)
+trim_graph.add_hline(
+    y=(sum(perc_kept_trim) / len(perc_kept_trim)),
+    line_width=1,
+    line_dash="dash",
+)
+trim_graph.update_layout(showlegend=False)
+trim_graph.update_xaxes(tickangle=55)
+
+# Quality filtering
+perc_kept_qf = (
+    report_sheet_dict["5_quality_filtering"]["passed reads"]
+    / report_sheet_dict["5_quality_filtering"]["processed reads"]
+    * 100
+)
+perc_kept_qf.index = (
+    report_sheet_dict["5_quality_filtering"]["File"]
+    .str.replace("_PE", "")
+    .str.replace("_trimmed_filtered.fasta.gz", "")
+    .str.replace("_trimmed.fasta.gz", "")
+    .str.replace(".fasta.gz", "")
+    .str.replace("_filtered.fasta.gz", "")
+)  # type: ignore
+perc_kept_qf = perc_kept_qf.fillna(0)
+qf_graph = px.bar(
+    perc_kept_qf.sort_values(),
+    range_y=[0, 100],
+    labels={"value": "Reads kept [%]", "File": "Samples"},
+    title="Percentage of reads kept during quality filtering",
+    width=graph_width,
+)
+qf_graph.add_hline(
+    y=(sum(perc_kept_qf) / len(perc_kept_qf)),
+    line_width=1,
+    line_dash="dash",
+)
+qf_graph.update_layout(showlegend=False)
+qf_graph.update_xaxes(tickangle=55)
+
+# Raw number of reads
+num_reads_raw = report_sheet_dict["3_PE merging"]["processed reads"]
+num_reads_raw.index = (
+    report_sheet_dict["3_PE merging"]["File"]
+    .str.replace("_PE.fastq.gz", "")
+    .str.replace(".fastq.gz", "")
+)  # type: ignore
+ymax_reads = max(num_reads_raw)
+rawreads_graph = px.bar(
+    num_reads_raw.sort_values(),
+    labels={"value": "Number of reads", "File": "Samples"},
+    title="Raw number of reads",
+    range_y=[0, ymax_reads],
+    width=graph_width,
+)
+rawreads_graph.add_hline(
+    y=(sum(num_reads_raw) / len(num_reads_raw)), line_width=1, line_dash="dash"
+)
+rawreads_graph.update_layout(showlegend=False)
+rawreads_graph.update_xaxes(tickangle=55)
+
+# Number of reads after PE merging and quality filtering
+num_reads_filtered = report_sheet_dict["5_quality_filtering"]["passed reads"]
+num_reads_filtered.index = (
+    report_sheet_dict["5_quality_filtering"]["File"]
+    .str.replace("_PE", "")
+    .str.replace("_trimmed", "")
+    .str.replace("_filtered", "")
+    .str.replace(".fasta.gz", "")
+)  # type: ignore
+filteredreads_graph = px.bar(
+    num_reads_filtered.sort_values(),
+    labels={"value": "Number of reads", "File": "Samples"},
+    title="Number of reads after PE merging, trimming and quality filtering",
+    range_y=[0, ymax_reads],
+    width=graph_width,
+)
+filteredreads_graph.add_hline(
+    y=(sum(num_reads_filtered) / len(num_reads_filtered)),
+    line_width=1,
+    line_dash="dash",
+)
+filteredreads_graph.update_layout(showlegend=False)
+filteredreads_graph.update_xaxes(tickangle=55)
+
+# Number of ESVs per sample before LULU
+ymax_esvs = esv_prelulu_sums.max()
+prelulu_graph_esvs = px.bar(
+    esv_prelulu_sums.sort_values(),
+    labels={"value": "Number of ESVs", "index": "Samples"},
+    title="Number of ESVs per sample before LULU",
+    range_y=[0, ymax_esvs],
+    width=graph_width,
+)
+prelulu_graph_esvs.add_hline(
+    y=(sum(esv_prelulu_sums) / len(esv_prelulu_sums)),
+    line_width=1,
+    line_dash="dash",
+)
+prelulu_graph_esvs.update_layout(showlegend=False)
+prelulu_graph_esvs.update_xaxes(tickangle=55)
+
+# Number of ESVs per sample after LULU
+postlulu_graph_esvs = px.bar(
+    esv_postlulu_sums.sort_values(),
+    labels={"value": "Number of ESVs", "index": "Samples"},
+    title="Number of ESVs per sample after LULU",
+    range_y=[0, ymax_esvs],
+    width=graph_width,
+)
+postlulu_graph_esvs.add_hline(
+    y=(sum(esv_postlulu_sums) / len(esv_postlulu_sums)),
+    line_width=1,
+    line_dash="dash",
+)
+postlulu_graph_esvs.update_layout(showlegend=False)
+postlulu_graph_esvs.update_xaxes(tickangle=55)
+
+# Number of OTUs per sample before LULU
+ymax_otus = otu_prelulu_sums.max()
+prelulu_graph_otus = px.bar(
+    otu_prelulu_sums.sort_values(),
+    labels={"value": "Number of OTUs", "index": "Samples"},
+    title="Number of OTUs per sample before LULU",
+    range_y=[0, ymax_otus],
+    width=graph_width,
+)
+prelulu_graph_otus.add_hline(
+    y=(sum(otu_prelulu_sums) / len(otu_prelulu_sums)),
+    line_width=1,
+    line_dash="dash",
+)
+prelulu_graph_otus.update_layout(showlegend=False)
+prelulu_graph_otus.update_xaxes(tickangle=55)
+
+# Number of OTUs per sample after LULU
+postlulu_graph_otus = px.bar(
+    otu_postlulu_sums.sort_values(),
+    labels={"value": "Number of OTUs", "index": "Samples"},
+    title="Number of OTUs per sample after LULU",
+    range_y=[0, ymax_otus],
+    width=graph_width,
+)
+postlulu_graph_otus.add_hline(
+    y=(sum(otu_postlulu_sums) / len(otu_postlulu_sums)),
+    line_width=1,
+    line_dash="dash",
+)
+postlulu_graph_otus.update_layout(showlegend=False)
+postlulu_graph_otus.update_xaxes(tickangle=55)
+
+# Number of reads vs number of ESVs
+reads_esvs_graph = px.scatter(
+    pd.concat([num_reads_filtered, esv_postlulu_sums], axis=1).reset_index(),
+    x="passed reads",
+    y="ESVs postlulu",
+    trendline="ols",
+    trendline_color_override="black",
+    hover_name="index",
+    title=project_name,
+    width=480,
+    labels={
+        "ESVs postlulu": "Number of ESVs after LULU",
+        "passed reads": "Number of filtered reads",
+    },
+)
+
+# Number of reads vs number of OTUs
+reads_otus_graph = px.scatter(
+    pd.concat([num_reads_filtered, otu_postlulu_sums], axis=1).reset_index(),
+    x="passed reads",
+    y="OTUs postlulu",
+    trendline="ols",
+    trendline_color_override="black",
+    hover_name="index",
+    title=project_name,
+    width=480,
+    labels={
+        "OTUs postlulu": "Number of OTUs after LULU",
+        "passed reads": "Number of filtered reads",
+    },
+)
+
+
+# Save graphs
+## Make outdir for project_name if it doesn't already exist
+outdir = os.path.join(f"{project_name}_apscale", "processing_graphs")
+os.makedirs(outdir, exist_ok=True)
+
+pe_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_1_pe_merging.{graph_format}",
+    )
+)
+trim_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_2_trimming.{graph_format}",
+    )
+)
+qf_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_3_qualityFiltering.{graph_format}",
+    )
+)
+rawreads_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_4_numberRawreads.{graph_format}",
+    )
+)
+filteredreads_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_5_numberFilteredreads.{graph_format}",
+    )
+)
+prelulu_graph_esvs.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_6_prelulu_esvs.{graph_format}",
+    )
+)
+postlulu_graph_esvs.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_7_postlulu_esvs.{graph_format}",
+    )
+)
+prelulu_graph_otus.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_8_prelulu_otus.{graph_format}",
+    )
+)
+postlulu_graph_otus.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_9_postlulu_otus.{graph_format}",
+    )
+)
+reads_esvs_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_10_reads_vs_esvs.{graph_format}",
+    )
+)
+reads_otus_graph.write_image(
+    os.path.join(
+        outdir,
+        f"{project_name}_11_reads_vs_otus.{graph_format}",
+    )
+)
---
+++ apscale_wrapper.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+
+"""
+A wrapper to run apscale on forward and reverse reads, generate processing QC graphs, and assign taxonomy to generated taxonomic units.
+
+Requires the submodules settings_generator.py and XXX in path.
+
+By Chris Hempel (christopher.hempel@kaust.edu.sa) on 15 Aug 2023
+"""
+
+import datetime
+import pandas as pd
+import argparse
+import warnings
+import subprocess
+from pathlib import Path
+
+# Define that warnings are not printed to console
+warnings.filterwarnings("ignore")
+
+
+# Funtion to print datetime and text
+def time_print(text):
+    datetime_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{datetime_now}  ---  " + text)
+
+
+# Function to generate an apscale Settings.xlsx file
+def generateSettings(**kwargs):
+    with pd.ExcelWriter(
+        Path(f"{project_name}_apscale").joinpath("Settings.xlsx"),
+        mode="w",
+        engine="openpyxl",
+    ) as writer:
+        # ## write the 0_general_settings sheet
+        df_0 = pd.DataFrame(
+            [[cores, 9]],
+            columns=["cores to use", "compression level"],
+        )
+
+        df_0.to_excel(writer, sheet_name="0_general_settings", index=False)
+
+        ## write the 3_PE_merging sheet
+        df_3 = pd.DataFrame(
+            [[25, 199, 5]], columns=["maxdiffpct", "maxdiffs", "minovlen"]
+        )
+
+        df_3.to_excel(writer, sheet_name="3_PE_merging", index=False)
+
+        ## write the 4_primer_trimming sheet
+        df_4 = pd.DataFrame(
+            [[forward_primer, reverse_primer, "False"]],
+            columns=["P5 Primer (5' - 3')", "P7 Primer (5' - 3')", "anchoring"],
+        )
+
+        df_4.to_excel(writer, sheet_name="4_primer_trimming", index=False)
+
+        ## write the 5_quality_filtering sheet
+        df_5 = pd.DataFrame(
+            [[maxEE, min_length, max_length]],
+            columns=["maxEE", "min length", "max length"],
+        )
+
+        df_5.to_excel(writer, sheet_name="5_quality_filtering", index=False)
+
+        ## write the 6_dereplication_pooling sheet
+        df_6 = pd.DataFrame([[4]], columns=["min size to pool"])
+
+        df_6.to_excel(writer, sheet_name="6_dereplication_pooling", index=False)
+
+        ## write the 7_otu_clustering sheet
+        df_7 = pd.DataFrame([[otu_perc, "True"]], columns=["pct id", "to excel"])
+
+        df_7.to_excel(writer, sheet_name="7_otu_clustering", index=False)
+
+        ## write the 8_denoising sheet
+        df_8 = pd.DataFrame([[2, 8, "True"]], columns=["alpha", "minsize", "to excel"])
+
+        df_8.to_excel(writer, sheet_name="8_denoising", index=False)
+
+        ## write the 8_denoising sheet
+        df_9 = pd.DataFrame(
+            [[84, 95, 1, "True"]],
+            columns=[
+                "minimum similarity",
+                "minimum relative cooccurence",
+                "minimum ratio",
+                "to excel",
+            ],
+        )
+
+        df_9.to_excel(writer, sheet_name="9_lulu_filtering", index=False)
+
+
+# Define arguments
+parser = argparse.ArgumentParser(
+    description="""A wrapper to run apscale on forward and reverse reads, generate processing QC graphs,
+    and assign taxonomy to generated taxonomic units.""",
+)
+parser.add_argument(
+    "-s",
+    "--sequence_dir",
+    help="""Path to directory containing demultiplexed forward and reverse sequences in .fastq.gz format.
+    Note: file names must end with R1 and R2 to identify read pairs.""",
+    required=True,
+)
+parser.add_argument(
+    "-p",
+    "--project_name",
+    help="Name of the apscale project to be generated.",
+    required=True,
+)
+parser.add_argument(
+    "-f",
+    "--forward_primer",
+    help="Forward primer sequence to trim.",
+    required=True,
+)
+parser.add_argument(
+    "-r",
+    "--reverse_primer",
+    help="Reverse primer sequence to trim.",
+    required=True,
+)
+parser.add_argument(
+    "-m",
+    "--min_length",
+    type=int,
+    help="Minimum limit of expected amplicon length (used for length filtering).",
+    required=True,
+)
+parser.add_argument(
+    "-M",
+    "--max_length",
+    type=int,
+    help="Maximum limit of expected amplicon length (used for length filtering).",
+    required=True,
+)
+parser.add_argument(
+    "-o",
+    "--otu_perc",
+    default=97,
+    type=int,
+    help="OTU identify treshold for clustering (default=97).",
+)
+parser.add_argument(
+    "-e",
+    "--maxEE",
+    default=2,
+    type=int,
+    help="maxEE (maximum estimated error) value used for quality filtering (default=2).",
+)
+parser.add_argument(
+    "-n",
+    "--cores",
+    default=2,
+    type=int,
+    help="Number of cores to use (default=2).",
+)
+args = parser.parse_args()
+
+# Set arguments
+sequence_dir = args.sequence_dir
+project_name = args.project_name
+forward_primer = args.forward_primer
+reverse_primer = args.reverse_primer
+min_length = args.min_length
+max_length = args.max_length
+otu_perc = args.otu_perc
+maxEE = args.maxEE
+cores = args.cores
+
+### Start of pipeline
+
+# Create an apscale directory using bash
+subprocess.run(["apscale", "--create_project", project_name])
+
+# Generate symlinks to demultiplexed reads
+subprocess.run(
+    f"ln -s $(realpath {sequence_dir})/* {project_name}_apscale/2_demultiplexing/data",
+    shell=True,
+)
+
+# Generate a Settings.xlsx file with given parameters
+generateSettings(
+    project_name=project_name,
+    forward_primer=forward_primer,
+    reverse_primer=reverse_primer,
+    min_length=min_length,
+    max_length=max_length,
+    otu_perc=otu_perc,
+    maxEE=maxEE,
+    cores=cores,
+)
+
+# Run apscale
+subprocess.run(["apscale", "--run_apscale", f"{project_name}_apscale"])
+
+# Generate processing graphs using separate script
