a09e90512a9ebe1f0581f9d444ad048629830388
hempelc <chempel.work@gmail.com>
2025-05-30 14:52:12 -0400

    added debug statement

--- apscale_blast.py
+++ apscale_blast.py
@@ -430,7 +430,7 @@ elif args.blast_filter_mode == "strict":
 
 # Process and save df
 df = post_processing(df)
-df = df.drop("percentage_similarity", axis=1)
+print(list(df.columns))
 df.to_csv(args.outfile, index=False)
 
 time_print("BLAST filtering done.")
